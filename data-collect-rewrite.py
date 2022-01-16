339012442b4042efa16c02b608407f9e6320889d
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-01-16 21:35:08 +0100

    Create data-collect-rewrite.py

---
+++ data-collect-rewrite.py
@@ -0,0 +1,78 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from time import sleep, time
+from lxml import html
+import sys
+
+
+def clean(file) -> None:
+    with open(file, "r+", encoding="utf-8") as f:
+        content = f.read()
+        content_clean1 = content.replace("'", "")
+        content_clean2 = content_clean1.replace(
+            ", [],", ", ???,").replace("[],", "").replace("[]", "")
+        content_clean3 = content_clean2.replace(" ???,  \n", "").replace(
+            "\n\n\n\n\n\n", "\n\n---, New Search, Quarry, ---\n\n")
+
+    with open("clean" + file.replace("./", ""), "w+", encoding="utf-8") as f:
+        f.write(content_clean3)
+
+
+def fetch_urls(keywords, *args):
+    for keyword in keywords:
+        for page_nb in range(1, 10):
+            driver.get(
+                "https://www.amazon.com/s?k={}&page={}".format(keyword, page_nb))
+            sleep(1)
+
+            tree = html.fromstring(driver.page_source)
+
+            for x, product_tree in enumerate(tree.xpath('//div[contains(@data-cel-widget, "search_result_")]')):
+                title = product_tree.xpath(
+                    './/span[@class="a-size-medium a-color-base a-text-normal"]/text()')
+                reviews = product_tree.xpath(
+                    './/span[@class="a-size-base"]/text()')
+                price = product_tree.xpath(
+                    './/span[@class="a-offscreen"]/text()')
+                links = list(product_tree.iterlinks())
+                (_, _, link, _) = links[0]
+                strLink = "amazon.com/" + link
+                listLink = [strLink]
+                link = (listLink if len(price)
+                        > 0 or len(title) > 0 else [])
+
+                title = str(title[0]).replace(",", "") if len(title) > 0 else str(
+                    title).replace(",", "")
+                reviews = str(reviews[0]).replace(",", ".") if len(reviews) > 0 else str(
+                    reviews).replace(",", ".")
+                price = str(price[0]).replace("\xa0", "").replace(",", ".").replace("'", "") if len(price) > 0 else str(
+                    price).replace(",", ".").replace("\xa0", "").replace("'", "")
+                link = str(link[0]).replace("'", "") if len(link) > 0 else str(
+                    link).replace("'", "")  # .replace(",", "")
+
+                info = str((title, reviews, price, link)).replace(
+                    "(", "").replace(")", "")
+                with open("./urls.csv", "a", encoding="utf-8") as f:
+                    f.write(str(info).replace(
+                        "(", "").replace(")", "") + "\n")
+
+        with open("./urls.csv", "a", encoding="utf-8") as f:
+            f.write("\n\n\n\n\n\n")
+    driver.close()
+    sleep(1)
+    clean("./urls.csv")
+    runTime = time() - startTime
+    sys.exit("Finished in {}".format(runTime))
+
+
+with open("./urls.csv", "w", encoding="utf-8") as f:
+    f.write("name, reviews, price, link\n")
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(options=options)
+print("Start")
+startTime = time()
+fetch_urls(["1. Arg", "2. Arg", "3. Arg", "..."])
