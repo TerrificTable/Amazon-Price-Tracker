046e9e8b211f6b29a40b5e27e821360f5d5f59c2
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-01-18 20:26:50 +0100

    now it actually works good

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import os
 
@@ -6,42 +7,64 @@ price = []
 reviews = []
 
 
-for file in os.listdir("./cleancsv/"):
-    with open("./cleancsv/{}".format(file), "r", encoding="utf-8") as f:
-        lines = f.readlines()
+def work(lines, name=None):
+    for i, line in enumerate(lines):
+        if i > 0:
+            line = line.split(", ")
+            data.append(line)
+            try:
+                if line[2] != "???" and line[2] != "Quarry" and not line[2].__contains__("amazon.com"):
+                    price.append(line[2])
+                if line[1] != "New Search" and not line[1].__contains__("amazon.com"):
+                    reviews.append(
+                        line[1] if not line[1] == "[]" and line[1] != "???" else "0")
+            except:
+                pass
 
-        for i, line in enumerate(lines):
-            if i > 0:
-                line = line.split(", ")
-                data.append(line)
-                try:
-                    if line[2] != "???" and line[2] != "Quarry" and not line[2].__contains__("amazon.com"):
-                        price.append(line[2])
-                    if line[1] != "New Search" and not line[1].__contains__("amazon.com"):
-                        reviews.append(
-                            line[1] if not line[1] == "[]" and line[1] != "???" else "0")
-                except:
-                    pass
+    newPrice = []
+    newReviews = []
+    for p in price:
+        p = p.split(".")
+        newPrice.append(
+            int(p[0].replace("$", "") if not p[0] == "\n" else 0))
+    for r in reviews:
+        newReviews.append(int(r.replace(".", "").replace("$", "")))
 
-        newPrice = []
-        newReviews = []
-        for p in price:
-            p = p.split(".")
-            newPrice.append(
-                int(p[0].replace("$", "") if not p[0] == "\n" else 0))
-        for r in reviews:
-            newReviews.append(int(r.replace(".", "").replace("$", "")))
-
-        avgprice = np.mean(newPrice)
-        avgrev = np.mean(newReviews)
-        print("Average Price of \"{}\": ".format(
-            file.replace("clean-data-", "")
-                .replace("_", " ")
-                .replace(".csv", "")
-        ) + str(avgprice.round(2)))
-
-        print("Average Reviews of \"{}\": ".format(
-            file.replace("clean-data-", "")
+    avgprice = np.mean(newPrice)
+    avgrev = np.mean(newReviews)
+    print("Average Price of \"{}\": ".format(
+        name.replace("clean-data-", "")
             .replace("_", " ")
             .replace(".csv", "")
-        ) + str(avgrev.round(1)) + "\n")
+    ) + str(avgprice.round(2)))
+
+    print("Average Reviews of \"{}\": ".format(
+        name.replace("clean-data-", "")
+        .replace("_", " ")
+        .replace(".csv", "")
+    ) + str(avgrev.round(1)) + "\n")
+
+
+def dir():
+    for file in os.listdir("./cleancsv/"):
+        with open("./cleancsv/{}".format(file), "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            work(lines, file)
+    sys.exit("Finished")
+
+
+# def arg():
+#     if len(sys.argv) > 1:
+#         if sys.argv[1] == "-f":
+#             files = sys.argv[::2]
+#             for file in files:
+#                 with open("./" + file.replace("./", ""), "r", encoding="utf-8") as f:
+#                     lines = f.readlines()
+#                     work(lines, file.replace("./", ""))
+#             sys.exit("Finished")
+
+
+if __name__ == "__main__":
+    print("Started")
+    # arg()
+    dir()
